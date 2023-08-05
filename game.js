3ebb27c82ade95d2aad495c6313b1af8df2a27e4
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2023-08-04 17:56:33 -0700

    added a way for player to pick option
    need to add a game function to decide a winner
    and another function to play five games

--- game.js
+++ game.js
@@ -1,11 +1,40 @@
 let choices = ['rock','paper','scissors']
-let choice;
+let computerChoice;
+let playerChoice;
+let playerWins;
+let computerWins;
+
 
 function getComputerChoice()
 {
-    choice = choices[(Math.floor(Math.random() * choices.length))];
-    return choice;
+    computerChoice = choices[(Math.floor(Math.random() * choices.length))];
+    computerChoice = computerChoice.toLowerCase();
+    return computerChoice;
 }
 
-getComputerChoice();
-console.log(choice);
+function getPlayerSelection()
+{
+    console.log(choices + '. Here are the choices, type your choice now');
+    while (true) {
+        playerChoice = prompt(choices).toLowerCase();
+        if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+          break; // Exit the loop if a valid choice is entered
+        } else {
+          alert("Invalid choice. Please enter rock, paper, or scissors.");
+        }
+      }
+}
+
+function playGame(playerChoice , computerChoice)
+{
+    if (playerChoice == 'rock' && computerChoice == 'scissors' || playerChoice == 'paper' && computerChoice == 'rock' || playerChoice == 'scissors' && computerChoice == 'paper')
+    {
+        playerWins++;
+        return"You've Won, you have " + playerWins + " , the computer has " + computerWins;
+    }
+
+    else if (playerChoice === computerChoice)
+    {
+        return"It was a tie, you have " + playerWins + " , the computer has " + computerWins;
+    }
+}
