842abba7c4857009e49bfc8ededfdce4d9245963
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2023-08-09 16:51:46 -0700

    Finished the game,
    could make the website look nicer and the images nicer

--- game.js
+++ game.js
@@ -5,6 +5,9 @@ let computerWins = 0;
 const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
 const scissorsButton = document.querySelector('#scissors');
+const winsText = document.querySelector('#wins')
+const choiceText = document.querySelector('#choiceText')
+const resetButton = document.querySelector('#resetButton')
 
 rockButton.addEventListener("click", function() {
     playRound('rock');
@@ -18,26 +21,45 @@ scissorsButton.addEventListener("click", function() {
     playRound('scissors');
 });
 
+resetButton.addEventListener('click', resetGame);
+function resetGame() {
+    playerWins = 0;
+    computerWins = 0;
+    winsText.textContent = "Player: " + playerWins + ", Computer: " + computerWins;
+    choiceText.textContent = "Make a choice!";
+}
+
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerChoice) {
     const computerChoice = getComputerChoice();
-    console.log("You chose " + playerChoice + " and the computer chose " + computerChoice);
+    choiceText.textContent = "You chose " + playerChoice + " and the computer chose " + computerChoice;
 
-    if (playerChoice === computerChoice) {
-        console.log("It was a tie!");
+    if (playerWins === 5)
+    {
+        winsText.textContent = "You've won the Best out of 5";
+        resetGame();
+
+    } 
+    else if (computerWins === 5)
+    {
+        winsText.textContent = "You've lost the Best out of 5";
+        resetGame();
+
+    }else if (playerChoice === computerChoice) {
+        winsText.textContent = "It was a tie! Player: " + playerWins + ", Computer: " + computerWins;
     } else if (
         (playerChoice === 'rock' && computerChoice === 'scissors') ||
         (playerChoice === 'paper' && computerChoice === 'rock') ||
         (playerChoice === 'scissors' && computerChoice === 'paper')
     ) {
         playerWins++;
-        console.log("You've won! Player: " + playerWins + ", Computer: " + computerWins);
+        winsText.textContent = "You've won! Player: " + playerWins + ", Computer: " + computerWins;
     } else {
         computerWins++;
-        console.log("You lost. Player: " + playerWins + ", Computer: " + computerWins);
+        winsText.textContent = "You lost. Player: " + playerWins + ", Computer: " + computerWins;
     }
 
     // Optionally, you can update the game status here if needed
